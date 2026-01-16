be9924d2acc9dfa91a527cdf482d3197a6f1d608
DragonCanFlies <lauratabay@hotmail.fr>
2026-01-16 14:36:07 +0100

    norm render_texture.c

--- src/render/render_texture.c
+++ src/render/render_texture.c
@@ -1,34 +1,30 @@
 #include "cub3d.h"
 
-static void	get_wall_orientation(t_game *g, t_ray *ray, t_wall* wall)
+static void	get_wall_orientation(t_game *g, t_ray *ray, t_wall *wall)
 {
 	if (ray->dis_v < ray->dis_h)
 	{
-		// vertical wall facing west
 		wall->text = g->tex.west;
 		wall->wall_dir = W;
 		if (cos(deg_to_rad(ray->ra)) < -0.001)
 		{
-			// vertical wall facing east
 			wall->text = g->tex.east;
 			wall->wall_dir = E;
 		}
 	}
 	else
 	{
-		// horizontal wall facing south
 		wall->text = g->tex.south;
 		wall->wall_dir = S;
 		if (sin(deg_to_rad(ray->ra)) < -0.001)
 		{
-			// horizontal wall facing north
 			wall->text = g->tex.north;
 			wall->wall_dir = N;
 		}
 	}
 }
 
-static void	compute_tx(t_ray *ray, t_wall* wall)
+static void	compute_tx(t_ray *ray, t_wall *wall)
 {
 	if (wall->wall_dir == W)
 		wall->tx = ((int) ray->v_hit_y & (MAP_S - 1)) * wall->text.map_scale;
@@ -46,7 +42,7 @@ static void	compute_tx(t_ray *ray, t_wall* wall)
 	}
 }
 
-static void	compute_ty(t_wall* wall)
+static void	compute_ty(t_wall *wall)
 {
 	wall->ty_off = 0;
 	wall->ty_step = (float)wall->text.height / (float)wall->line_h;
@@ -59,7 +55,7 @@ static void	compute_ty(t_wall* wall)
 	wall->line_offset = (WIN_HEIGHT >> 2) - (wall->line_h >> 1);
 }
 
-static void	check_boundaries(t_wall* wall)
+static void	check_boundaries(t_wall *wall)
 {
 	if (wall->tx < 0)
 		wall->tx = 0;
@@ -71,32 +67,26 @@ static void	check_boundaries(t_wall* wall)
 		wall->ty = wall->text.height - 1;
 }
 
-static void	draw_wall(t_game *g, t_ray *ray, t_wall* wall)
+static void	draw_wall(t_game *g, t_ray *ray, t_wall *wall)
 {
 	int		i;
 	int		color;
-	int		tex_x;
-	int		tex_y;
 	int		x;
 	int		x1;
 	t_line	line;
 
-	// Use px_per_ray from game struct (now a float!)
- 	x = (int)(ray->index * g->px_per_ray);
- 	x1 = (int)((ray->index + 1) * g->px_per_ray) - 1;  // ← Same fix!
- 
- 	// Safety bounds
- 	if (x1 >= WIN_WIDTH)
-  		x1 = WIN_WIDTH - 1;
-
+	x = (int)(ray->index * g->px_per_ray);
+	x1 = (int)((ray->index + 1) * g->px_per_ray) - 1;
+	if (x1 >= WIN_WIDTH)
+		x1 = WIN_WIDTH - 1;
 	i = 0;
 	while (i < wall->line_h)
 	{
-		tex_x = (int)wall->tx & (wall->text.height - 1);
-		tex_y = (int)wall->ty & (wall->text.height - 1);
-		color = *(unsigned int *)(wall->text.buffer 
-			+ tex_y * wall->text.line_len 
-			+ tex_x * (wall->text.bpp >> 3));
+		color = *(unsigned int *)(wall->text.buffer
+				+ ((int)wall->ty & (wall->text.height - 1))
+				* wall->text.line_len
+				+ ((int)wall->tx & (wall->text.height - 1))
+				* (wall->text.bpp >> 3));
 		line = init_line(x, wall->line_offset + i, x1, wall->line_offset + i);
 		draw_line(&g->img, &line, color);
 		wall->ty += wall->ty_step;
@@ -104,7 +94,7 @@ static void	draw_wall(t_game *g, t_ray *ray, t_wall* wall)
 	}
 }
 
-void	apply_texture(t_game *g, t_ray *ray, t_wall* wall)
+void	apply_texture(t_game *g, t_ray *ray, t_wall *wall)
 {
 	get_wall_orientation(g, ray, wall);
 	compute_tx(ray, wall);
